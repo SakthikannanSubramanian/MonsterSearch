70e1e0e83aa15dd4c26f86702ed456c251934c10
Sakthikannan Subramanian <sakthikannan.subramanian@waitrose.co.uk>
2020-04-09 22:39:41 +0100

    Updates

---
+++ precache-manifest.54e0453f020e18955af0171c7ca55f4b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62c06846859621fc70803efb47a53ad4",
+    "url": "/MonsterSearch/index.html"
+  },
+  {
+    "revision": "b299e3878b1d5f0e40d1",
+    "url": "/MonsterSearch/static/css/main.b314c918.chunk.css"
+  },
+  {
+    "revision": "1bb5dd2a6657ff2ee685",
+    "url": "/MonsterSearch/static/js/2.b6f2604e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/MonsterSearch/static/js/2.b6f2604e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b299e3878b1d5f0e40d1",
+    "url": "/MonsterSearch/static/js/main.c3893cd7.chunk.js"
+  },
+  {
+    "revision": "ceb51a964bfc37200013",
+    "url": "/MonsterSearch/static/js/runtime-main.a8461c2e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/MonsterSearch/static/media/logo.5d5d9eef.svg"
+  }
+]);
